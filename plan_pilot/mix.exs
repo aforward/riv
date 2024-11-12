17ae3c805eead01a397a1778b5394f35e2b3afc6
Andrew Forward <aforward@hey.com>
2024-11-12 15:59:56 -0500

    Add kore to plan_pilot project

--- plan_pilot/mix.exs
+++ plan_pilot/mix.exs
@@ -32,6 +32,7 @@ defmodule PlanPilot.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:kore, path: "../libs/kore"},
       {:phoenix, "~> 1.7.14"},
       {:phoenix_ecto, "~> 4.5"},
       {:ecto_sql, "~> 3.10"},
